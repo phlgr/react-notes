161128edcb3d34eb452626766e62caeb5ca4eaf4
phlgr <philippm.gartz@gmail.com>
2020-03-14 20:19:53 +0100

    Add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './App.css';
+import Header from './Header';
+import Button from './components/Button';
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Link to="/">
+        <Header />
+      </Link>
+      <Switch>
+        <Route exact path="/">
+          <Button>Hallo!</Button>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
